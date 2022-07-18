e10a667869f454bb8a83f3318ecdfd704afb9a26
sbx_user1051 <sbx_user1051@169.254.102.241>
2022-07-18 18:03:29 +0000

    Mon Jul 18 18:03:29 UTC 2022

--- test/solution_tests/CHK/test_two.py
+++ test/solution_tests/CHK/test_two.py
@@ -3,22 +3,27 @@ import pytest
 from lib.solutions.CHK.checkout_solution import checkout, offer
 
 
-@pytest.mark.parametrize("test_input,result",[
-    ("A", 50),
-    ("AA", 100),
-    ("AAA", 130),
-    ("AAAA", 180),
-    ("AAAAA", 200),
-    ("AAAAAA", 250),
+@pytest.mark.parametrize("test_input,char,result",[
+    ("A","A", 50),
+    ("AA", "A",100),
+    ("AAA", "A",130),
+    ("AAAA", "A",180),
+    ("AAAAA", "A",200),
+    ("AAAAAA", "A",250),
+    ("B","B",30),
+    ("BB","B",45),
+    ("AABB","B",45)
+    
 ])
-def test_offer(test_input, result):
+def test_offer(test_input, char, result):
     assert offer(
         test_input,
-        "A",
+        char,
         [
             (1, 50),
             (3, 130),
             (5, 200)
         ]) == result
+
 
 
