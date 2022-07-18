1e567be7a347897ba5755baf20d5e7eb3b98cfda
sbx_user1051 <sbx_user1051@169.254.102.241>
2022-07-18 18:24:15 +0000

    Mon Jul 18 18:24:15 UTC 2022

---
+++ test/solution_tests/CHK/test_four.py
@@ -0,0 +1,41 @@
+import pytest
+
+from lib.solutions.CHK.checkout_solution import checkout, offer
+
+
+@pytest.mark.parametrize("test_input,result",[
+    (1, 50),
+    (2,100),
+    (3,130),
+    (4,180),
+    (5,200),
+    (6,250),
+])
+def test_offer(test_input, result):
+    assert offer(
+        test_input,
+        [
+            (1, 50),
+            (3, 130),
+            (5, 200)
+        ]) == result
+
+
+@pytest.mark.parametrize("test_input,result",[
+    ("AAA", 130),
+    ("B", 30),
+    ("BB", 45),
+    ("BBBE", 115),
+    ("BBBEE", 125),
+    ("BBBEe", -1),
+    ("C", 20),
+    ("D", 15),
+    ("F", 10),
+    ("FF", 20),
+    ("FFF", 20),
+    ("AAAFF", 150),
+    ("AAAFFF", 150),
+
+])
+def test_checkout(test_input, result):
+    assert checkout(test_input) == result
