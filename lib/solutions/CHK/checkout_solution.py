a6bec94761b4bbba421450eb926194b5828c3749
sbx_user1051 <sbx_user1051@169.254.102.241>
2022-07-18 17:11:39 +0000

    Mon Jul 18 17:11:39 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,8 +7,32 @@ def checkout(skus):
     if not isinstance(skus, str):
         return -1
     
+    count = {
+        "A" : 0,
+        "B" : 0,
+        "C" : 0,
+        "D" : 0
+    }
+
     for char in skus:
-        if char not in ["A","B","C","D"]:
+        if char not in count.keys():
             return -1
+        count[char] += 1
+
+    #value counts
+    ret = 15 * count["D"] + 20 * count["C"]
+
+    modulo, remainder = divmod(count["A"], 3)
+    ret += 130 * modulo + 50 * remainder
+
+    modulo, remainder = divmod(count["B"], 2)
+    ret += 45 * modulo + 30 * remainder
+
+    return ret
+
+
+
+
+
 
 
