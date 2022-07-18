9d58fb33b11a477e1e1be6f8c8cbe9fe6acc180a
sbx_user1051 <sbx_user1051@169.254.144.33>
2022-07-18 15:17:31 +0000

    Mon Jul 18 15:17:31 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,10 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def compute(x: int, y: int):
+
+    if not isinstance(x, int) or not isinstance(y, int):
+        raise TypeError(f"received non-integer argument")
+    elif x not in range(0, 101) or y not in range(0, 101):
+        raise ValueError(f"argument not in range 0-100")
+
+    return x + y
+
