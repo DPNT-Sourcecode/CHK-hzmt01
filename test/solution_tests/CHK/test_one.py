38ea8d001f61174e33fbc159427bbdcd20fca164
sbx_user1051 <sbx_user1051@169.254.102.241>
2022-07-18 17:18:27 +0000

    Mon Jul 18 17:18:27 UTC 2022

--- test/solution_tests/CHK/test_one.py
+++ test/solution_tests/CHK/test_one.py
@@ -10,12 +10,13 @@ from lib.solutions.CHK.checkout_solution import checkout
     ("AAAA", 180),
     ("B", 30),
     ("BB", 45),
-    ("BBB", 75)
+    ("BBB", 75),
     ("C", 20),
     ("CC", 40),
     ("D", 15),
     ("DD", 30),
+    ("a", -1),
+    ("ABCD",115)
 ])
 def test_checkout(test_input, result):
     assert checkout(test_input) == result
-
